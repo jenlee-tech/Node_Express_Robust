921d813329145d0307e13cbd0fcb449290edb374
Jennifer Lee <jenlee.tech@gmail.com>
2022-03-03 22:46:42 -0500

    included a bodyDataHas function in pastes.controller

--- src/pastes/pastes.controller.js
+++ src/pastes/pastes.controller.js
@@ -4,19 +4,72 @@ function list(req, res) {
   res.json({ data: pastes });
 }
 
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+// New middleware function to validate the request body - old validation
+// function bodyHasTextProperty(req, res, next) {
+//   const { data: { text } = {} } = req.body;
+//   if (text) {
+//     return next(); // Call `next()` without an error message if the result exists
+//   }
+//   next({
+//     status: 400,
+//     message: "A 'text' property is required.",
+//   });
+// }
 
-// New middleware function to validate the request body
-function bodyHasTextProperty(req, res, next) {
-  const { data: { text } = {} } = req.body;
-  if (text) {
-    return next(); // Call `next()` without an error message if the result exists
+//this function validates any property (key-value pair) that the developer wants but then exports towards the end of the file.
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Must include a ${propertyName}` });
+  };
+}
+
+function exposurePropertyIsValid(req, res, next) {
+  const { data: { exposure } = {} } = req.body;
+  const validExposure = ["private", "public"];
+  if (validExposure.includes(exposure)) {
+    return next();
   }
   next({
     status: 400,
-    message: "A 'text' property is required.",
+    message: `Value of the 'exposure' property must be one of ${validExposure}. Received: ${exposure}`,
   });
 }
+
+function syntaxPropertyIsValid(req, res, next) {
+  const { data: { syntax } = {} } = req.body;
+  const validSyntax = [
+    "None",
+    "Javascript",
+    "Python",
+    "Ruby",
+    "Perl",
+    "C",
+    "Scheme",
+  ];
+  if (validSyntax.includes(syntax)) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Value of the 'syntax' property must be one of ${validSyntax}. Received: ${syntax}`,
+  });
+}
+
+function expirationIsValidNumber(req, res, next) {
+  const { data: { expiration } = {} } = req.body;
+  if (expiration <= 0 || !Number.isInteger(expiration)) {
+    return next({
+      status: 400,
+      message: `Expiration requires a valid number`,
+    });
+  }
+  next();
+}
+
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
 
 function create(req, res) {
@@ -35,7 +88,19 @@ function create(req, res) {
   res.status(201).json({ data: newPaste });
 }
 
+//create: this puts the middle ware and the route handler together in an array.
 module.exports = {
-  create: [bodyHasTextProperty, create],
+  create: [
+    bodyDataHas("name"),
+    bodyDataHas("syntax"),
+    bodyDataHas("exposure"),
+    bodyDataHas("expiration"),
+    bodyDataHas("text"),
+    bodyDataHas("user_id"),
+    exposurePropertyIsValid,
+    syntaxPropertyIsValid,
+    expirationIsValidNumber,
+    create,
+  ],
   list,
 };
