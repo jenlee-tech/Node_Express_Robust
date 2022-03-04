65b919112af034e3d1b5ee4ad4d4450d395a4639
Jennifer Lee <jenlee.tech@gmail.com>
2022-03-04 16:30:38 -0500

    added the pastes router to users router for fun and testing

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -2,7 +2,14 @@ const router = require("express").Router();
 
 const controller = require("./users.controller");
 
-router.route("/:userId").get(controller.read);
-router.route("/").get(controller.list);
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+const pastesRouter = require("../pastes/pastes.router");
+
+router.use("/:userID/pastes", pastesRouter);
+
+router.route("/:userId").get(controller.read).all(methodNotAllowed);
+
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
